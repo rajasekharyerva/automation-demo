24bcf288848648e0901d2cbe115ebc09af84c91b
Y Rajasekhar Reddy <sekharan7@gmail.com>
2024-12-11 08:44:20 +0530

    added headless

--- src/test/java/com/automation/magento/stepDefinitions/Hooks.java
+++ src/test/java/com/automation/magento/stepDefinitions/Hooks.java
@@ -4,11 +4,14 @@ import io.cucumber.java.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.cucumber.java.After;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Hooks {
     static WebDriver webDriver;
     @Before
     public void openBrowser() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless");
         webDriver = new ChromeDriver();
         webDriver.manage().window().maximize();
     }
